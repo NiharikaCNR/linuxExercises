dd6c33d931969e4ea169f6931495476dffe516ee
Vaishnavi Borwankar <vborwankar@wisc.edu>
2023-10-19 21:17:05 -0500

    Added Q5 an Q6

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+mkdir -p five
+
+for i in {1..5};
+do
+    dir="five/dir$i"
+    mkdir -p "$dir"
+    for j in {1..4};
+    do
+	file="$dir/file$j"
+	content=""
+	for k in $(seq $j);
+	do
+	    content="${content}${j}\n"
+	done
+        echo -n -e "$content" > "$file"
+    done
+done
+
+
